5b1b29b13e9a3525ff4000d7d7299679bfaa1ae8
Bharath Sriraam R R <albharath.1305@gmail.com>
2026-03-13 15:01:52 -0400

    Restore parser format reward name

--- environments/chart_extraction/rubric/__init__.py
+++ environments/chart_extraction/rubric/__init__.py
@@ -9,11 +9,9 @@ from .rewards.series_name_f1 import series_name_f1
 rubric = vf.Rubric(parser=parser)
 rubric.parallelize_scoring = False
 rubric.class_objects["logger"] = rubric.logger
-format = parser.get_format_reward_func()
-format.__name__ = "format"
 
 rubric.add_reward_func(cache_parsed_answer, weight=0.0)
-rubric.add_reward_func(format, weight=1.0)
+rubric.add_reward_func(parser.get_format_reward_func(), weight=1.0)
 rubric.add_reward_func(series_name_f1, weight=1.0)
 rubric.add_reward_func(series_point_count_ratio, weight=1.0)
 rubric.add_reward_func(series_point_value, weight=1.0)
